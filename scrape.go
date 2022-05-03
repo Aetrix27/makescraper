555706a509ea2b4164affebc9019879ffbcfe658
David <davedogg323@gmail.com>
2022-05-02 21:24:05 -0700

    organized data into struct

--- scrape.go
+++ scrape.go
@@ -8,8 +8,16 @@ import (
 	"github.com/gocolly/colly"
 )
 
-type data struct {
-	json string
+type stock_data struct {
+	in_stock      bool
+	stock_message string
+}
+
+func newStock() *stock_data {
+	stock := stock_data{}
+	stock.in_stock = false
+	stock.stock_message = ""
+	return &stock
 }
 
 // main() contains code adapted from example found in Colly's docs:
@@ -20,8 +28,9 @@ func main() {
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	var inStock bool
-	inStock = false
+	//var inStock bool
+	stock_check := newStock()
+	//inStock = false
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 
@@ -32,7 +41,7 @@ func main() {
 		//_ = inStock
 
 		if strings.Contains("select delivery location", e.Text) {
-			inStock = true
+			stock_check.in_stock = true
 
 		}
 	})
@@ -45,9 +54,11 @@ func main() {
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://www.amazon.com/Microsoft-Xbox-1TB-Compatible-Performance/dp/B09YTP3JS2/ref=sr_1_9?crid=133AZ4XGTLBIQ&keywords=xbox+series+x&qid=1651253940&sprefix=%2Caps%2C460&sr=8-9")
 
-	if inStock == true {
+	if stock_check.in_stock == true {
+		stock_check.stock_message = "The item is in stock!"
 		fmt.Println("The item is in stock!")
-	} else if inStock == false {
+	} else if stock_check.in_stock == false {
+		stock_check.stock_message = "The item is out of stock!"
 		fmt.Println("The item is out of stock!")
 	}
 
